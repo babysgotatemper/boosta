891f0864a01a16476acf9178fe30f1fc9cab3a58
RomanSemak <semak.roman@gmail.com>
2017-03-12 23:27:41 +0200

    added retina images and script

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,28 +1,27 @@
 $(document).ready(function () {
+    /*retina img*/
+    if (window.devicePixelRatio > 1) {
+        var lowresImages = $('img');
+
+        images.each(function (i) {
+            var lowres = $(this).attr('src');
+            var highres = lowres.replace(".", "@2x.");
+            $(this).attr('src', highres);
+        });
+    }
+
     $(" .user__rate").each(function () {
         var rateVal = $(this).find('.user__rate-num').html(),
             rateLine = $(this).find('.user__rate-progress .value');
         $(rateLine).width(rateVal + '%');
     });
+
     /*slider*/
-    //        $('.slider__prev').click(function(){
-    //            $('.slider__wrap .user:first-child').appendTo('.slider__wrap').fadeIn();
-    //        })
-    function showMessage() {
-        alert('Transition закончил свое выполнение');
-    }
     $('.slider__prev').click(function () {
         $('.slider__wrap .user:first-child').appendTo('.slider__wrap');
     })
     $('.slider__next').click(function () {
-            $('.slider__wrap .user:last-child').prependTo('.slider__wrap').fadeIn();
-        })
-        //    $('.slider__prev').click(function () {
-        //        $('.slides').animate({
-        //            'left': step
-        //        }, 'slow', function () {
-        //            $('.slides li:last-child').prependTo('.slides');
-        //            $('.slides').css('left', 0);
-        //        });
-        //    });
+        $('.slider__wrap .user:last-child').prependTo('.slider__wrap').fadeIn();
+    })
+
 });
